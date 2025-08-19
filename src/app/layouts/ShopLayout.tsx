061f7c654507f2cca1945ade7109b9a7daedc86c
agrade1 <kigf59@gmail.com>
2025-08-19 16:08:23 +0900

    feat:homepage 기본 레이아웃 구조 목업

---
+++ src/app/layouts/ShopLayout.tsx
@@ -0,0 +1,12 @@
+import Footer from '../../shared/ui/Footer';
+import Header from '../../shared/ui/Header';
+
+export default function ShopLayout() {
+  return (
+    <>
+      <Header />
+      shop layout
+      <Footer />
+    </>
+  );
+}
