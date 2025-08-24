2eeed290becbbdb0166107e15cce05c911b2f217
agrade1 <kigf59@gmail.com>
2025-08-25 02:49:42 +0900

    style: screen layout css 설정 및 공용 css 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+    screens: {
+      xs: '320px',
+      sm: '375px',
+      md: '768px',
+      lg: '1200px',
+    },
+  },
   plugins: [],
 };
