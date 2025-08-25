b7d119a6353af2005c1c53a6100a96a0a8fa302d
agrade1 <kigf59@gmail.com>
2025-08-26 03:36:18 +0900

    feat: header 레이아웃 추가 및 menu 컴포넌트 분할

--- src/app/layouts/_components/Header/Header.tsx
+++ src/app/layouts/_components/Header/Header.tsx
@@ -1,23 +1,61 @@
 import { NavLink } from 'react-router-dom';
-import logoSvg from '@/assets/images/icons/Logo.svg';
 import SearchInput from '@/shared/ui/SearchInput';
+import MenuList from './MenuList';
+import { useState } from 'react';
+import { BookIcon, ListIcon, ShoppingCartIcon, UserCircleIcon, XIcon } from '@phosphor-icons/react';
+import type { MenuItem } from '@/types/headerTypes';
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const menus: MenuItem[] = [
+    { id: 1, to: '/books', label: 'books', Icon: BookIcon },
+    { id: 2, to: '/cart', label: 'cart', Icon: ShoppingCartIcon },
+    { id: 3, to: '/user', label: 'user', Icon: UserCircleIcon },
+  ];
+
   return (
-    <header className="flex-center bg-black h-16">
-      <nav className=" max-w-[1440px] text-white">
-        <div className="flex-center">
-          <NavLink to="/" className="shrink-0 flex items-center gap-2">
+    <header>
+      <nav className="flex-between cont-wrap h-16 gap-4 border-b">
+        <div className="flex-center h-full gap-4">
+          <NavLink to="/" className="inline-flex items-center gap-2 text-lg font-semibold">
             Bindery
           </NavLink>
-          <SearchInput
-            onSearch={(q) => {
-              console.log('search:', q);
-            }}
-          />
+          <div className="hidden lg:block">
+            <SearchInput
+              onSearch={(q) => {
+                console.log('search:', q);
+              }}
+            />
+          </div>
         </div>
-        <div className="flex-center"></div>
+        <div className="h-full">
+          <MenuList menus={menus} />
+
+          <button
+            type="button"
+            className="inline-flex h-full cursor-pointer items-center justify-center rounded-md p-2 md:hidden"
+            onClick={() => setOpen(true)}
+          >
+            <ListIcon size={22} weight="bold" />
+          </button>
+        </div>
       </nav>
+      <div
+        className={`fixed top-0 right-0 z-50 flex h-full w-64 transform flex-col gap-2 bg-white shadow-lg transition-transform duration-300 ${open ? 'translate-x-0' : 'translate-x-full'} `}
+      >
+        <div className="flex-end h-16 border-b px-2">
+          <button
+            type="button"
+            className="mr-4 ml-auto shrink-0 cursor-pointer rounded-md lg:hidden"
+            onClick={() => setOpen(false)}
+          >
+            <XIcon size={22} weight="bold" />
+          </button>
+        </div>
+        <div className="px-2">
+          <MenuList menus={menus} isMobile={true} />
+        </div>
+      </div>
     </header>
   );
 }
