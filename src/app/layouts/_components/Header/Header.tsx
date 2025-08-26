ca443d4b6537fb1b47992dc4c07207b25d2b1bb2
agrade1 <kigf59@gmail.com>
2025-08-26 18:19:54 +0900

    feat: menus 배열 layout 으로 이동 및 props 내려주기, style 수정

--- src/app/layouts/_components/Header/Header.tsx
+++ src/app/layouts/_components/Header/Header.tsx
@@ -2,50 +2,49 @@ import { NavLink } from 'react-router-dom';
 import SearchInput from '@/shared/ui/SearchInput';
 import MenuList from './MenuList';
 import { useState } from 'react';
-import { BookIcon, ListIcon, ShoppingCartIcon, UserCircleIcon, XIcon } from '@phosphor-icons/react';
+import { ListIcon, XIcon } from '@phosphor-icons/react';
 import type { MenuItem } from '@/types/headerTypes';
 import { useOverlay } from '@/context/overlayContext';
 
-export default function Header() {
+type Props = { menus: MenuItem[] };
+export default function Header({ menus }: Props) {
   const [open, setOpen] = useState(false);
   const { isOverlay, setOverlay } = useOverlay();
   const moNavOpen = () => {
     setOpen(!open);
     setOverlay(!isOverlay);
   };
-  const menus: MenuItem[] = [
-    { id: 1, to: '/books', label: 'books', Icon: BookIcon },
-    { id: 2, to: '/cart', label: 'cart', Icon: ShoppingCartIcon },
-    { id: 3, to: '/user', label: 'user', Icon: UserCircleIcon },
-  ];
 
   return (
-    <header>
-      <nav className="flex-between cont-wrap h-16 gap-4 border-b">
-        <div className="flex-center h-full gap-4">
-          <NavLink to="/" className="inline-flex items-center gap-2 text-lg font-semibold">
-            Bindery
-          </NavLink>
-          <div className="hidden lg:block">
-            <SearchInput
-              onSearch={(q) => {
-                console.log('search:', q);
-              }}
-            />
+    <>
+      <header className="sticky top-0 z-40 bg-[#fffbf5]">
+        <nav className="flex-between cont-wrap h-16 gap-4 shadow-[0_4px_14px_0_#00000040]">
+          <div className="flex-center h-full gap-7">
+            <NavLink to="/" className="inline-flex items-center gap-2 text-2xl font-semibold">
+              Bindery
+            </NavLink>
+            <div className="hidden w-60 lg:block">
+              <SearchInput
+                onSearch={(q) => {
+                  console.log('search:', q);
+                }}
+              />
+            </div>
           </div>
-        </div>
-        <div className="h-full">
-          <MenuList menus={menus} />
+          <div className="h-full">
+            <MenuList menus={menus} />
 
-          <button
-            type="button"
-            className="inline-flex h-full cursor-pointer items-center justify-center rounded-md p-2 md:hidden"
-            onClick={() => moNavOpen()}
-          >
-            <ListIcon size={22} weight="bold" />
-          </button>
-        </div>
-      </nav>
+            <button
+              type="button"
+              className="inline-flex h-full cursor-pointer items-center justify-center rounded-md p-2 md:hidden"
+              onClick={() => moNavOpen()}
+            >
+              <ListIcon size={22} weight="bold" />
+            </button>
+          </div>
+        </nav>
+      </header>
+
       <div
         className={`fixed top-0 right-0 z-50 flex h-full w-64 transform flex-col gap-2 bg-white shadow-lg transition-transform duration-300 ${open ? 'translate-x-0' : 'translate-x-full'} `}
       >
@@ -62,6 +61,6 @@ export default function Header() {
           <MenuList menus={menus} isMobile={true} />
         </div>
       </div>
-    </header>
+    </>
   );
 }
